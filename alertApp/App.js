898036684d8ec55d15d26df29dbe3cf2111ba9f6
Igor Ryan <igorsonic56@gmail.com>
2025-11-01 10:56:47 +0530

    Subindo botões base e começo do desafio

---
+++ alertApp/App.js
@@ -0,0 +1,66 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Alert, Button, Pressable } from 'react-native';
+
+export default function App() {
+
+  const abrirAlerta = () =>{
+    Alert.alert( 'Olá, alerta com um botão')
+  }
+
+  const abrirAlertaComDoisBotoes = () => {
+    Alert.alert(
+    'Olá!','Alerta com dois botões',
+    [{text: 'Ok', onPress: () => console.log("Ok")},
+    {text: 'Cancelar', onPress: () => console.log("Cancelar"),},],
+    { cancelable: true });
+}
+
+const abrirAlertaComTresBotoes = () =>{
+   Alert.alert(
+    'Este é o alerta com 3 botões','Escolha uma das opções:',
+    [{text: 'Pergunte-me depois', onPress: ()=> console.log("Pergunte-me depois")},
+     {text: 'sim', onPress: ()=> console.log("Sim")},
+     {text: 'Não', onPress: ()=> console.log("Não")}],
+     {cancelable: false});
+}
+  return (
+    <View style={styles.container}>
+       <StatusBar style="auto" />
+
+      <Button title='Botão simples' onPress={abrirAlerta} style={styles.btnSimples}/>
+
+      <Button title='2 botões' onPress={abrirAlertaComDoisBotoes}/>
+
+      <Button title='3 botões' onPress={abrirAlertaComTresBotoes}/>
+
+      <View style={styles.desafio}>
+        <Text>PARTE DO DESAFIO</Text>
+
+        <Button title='Radom btn'/>
+      </View>
+
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  btnSimples: {
+    backgroundColor: 'red',
+  },
+
+  desafio:{
+    backgroundColor: 'yellow',
+    padding: 100,
+    borderRadius: 10,
+    marginTop: 10,
+    gap: 30,
+  },
+});
